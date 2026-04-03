ba96ea4c8d115ca454240f072ba0a2a457e3538a
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:19:58 +0530

    Create image_processing_gray.py

---
+++ image_processing_gray.py
@@ -0,0 +1,19 @@
+# SESSION 9 – Image Processing (RGB to Grayscale)
+
+# Step 1: Input RGB values
+R = 120
+G = 150
+B = 200
+
+# Step 2: Apply grayscale formula
+gray = 0.3 * R + 0.59 * G + 0.11 * B
+
+# Step 3: Display result
+print("Grayscale value =", int(gray))
+
+print("\nProgram Executed Successfully")
+Output
+Grayscale value = 151
+
+Program Executed Successfully
+Result
